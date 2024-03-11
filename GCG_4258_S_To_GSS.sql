a766ca6f05cf4fa65bf7b1bd7756b7d1c432e8d3
Service Web <sw@gssmail.com>
2024-03-11 14:40:14 -0500

    Commit changes in C:\Websites\Git\emarlow\4258\GCG_4258_S_To_GSS.sql

---
+++ GCG_4258_S_To_GSS.sql
@@ -0,0 +1,13 @@
+/* ==========================================
+ * TABLE: GCG_4258_S_To_GSS
+ * ========================================== */
+CREATE TABLE "GCG_4258_S_To_GSS"(
+ "JOB" CHAR(6),
+ "SUFFIX" CHAR(3),
+ "SEQUENCE" CHAR(6),
+ "MACHINE" CHAR(4),
+ "GOOD_PIECES" NUMERIC(12,4),
+ "BAD_PIECES" NUMERIC(12,4),
+ "REASON" CHAR(2));
+
+
